71e5d66f3aee75f1916de20f5945e02090016ac7
Neek FHD <neekkafle29@gmail.com>
2023-12-03 21:33:07 +0545

    Hero Section Dev

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,7 +3,15 @@ import '../Styles/Home.scss'
 const Home = () => {
   return (
     <div className='Home'>
-
+        <section className="hero_section">
+          <h1 className='hero_title'>Easy Attorney Service<br /><span className='hero_gradient'>Anywhere. Anytime.</span></h1>
+          <p className='hero_para1'>Connect to different attorneys respective to your legal problems.</p>
+          <div className="input_cta">
+            <input type="email" placeholder='Your email' className='cta_input' autoFocus/>
+            <button className='input_cta_button'>Get free consultation</button>
+          </div>
+          <p className='hero_para2'><span className='asterisk'>*</span> Get your free consultation today. No credit card required.</p>
+        </section>
     </div>
   )
 }
---
+++ client/src/Pages/HomeHeroPic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeHeroPic = () => {
+  return (
+    <div className='hero_pic'></div>
+  )
+}
+
+export default HomeHeroPic
