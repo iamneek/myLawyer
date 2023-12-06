701f1822c351fa33fd9463fc6272b1923950080b
Neek FHD <neekkafle29@gmail.com>
2023-12-06 14:32:27 +0545

    As Seen On Marquee

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,13 @@
 import Nav from '../Components/Nav'
 import '../Styles/Home.scss'
 import HomeHeroPic from '../Components/HomeHeroPic'
+import Marquee from "react-fast-marquee";
+import { FaGoogle, FaMicrosoft } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { SiCnn, SiFox, SiOpenai } from "react-icons/si";
+import { BsMeta } from "react-icons/bs";
+
+
 const Home = () => {
   return (
     <div className='Home'>
@@ -16,8 +23,22 @@ const Home = () => {
           {/* --- Hero Demo Video Start --- */}
           <HomeHeroPic /> 
           {/* --- Hero Demo Video End --- */}
-          
         </section>
+        <section className="marquee_as_seen_outer">
+          <div className="marquee_as_seen">
+            <span className='as_seen_on_heading'>AS SEEN ON</span>
+            <Marquee pauseOnHover gradient gradientColor={'#12141c'} autoFill={true}>
+              <div className='image_wrapper'><FaGoogle size={80} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><FaXTwitter size={80} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><SiCnn size={150} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><SiFox size={150} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><BsMeta size={100} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><FaMicrosoft size={80} color='#a6a6a6'/></div>
+              <div className='image_wrapper'><SiOpenai size={80} color='#a6a6a6'/></div>
+            </Marquee>
+          </div>
+        </section>
+
     </div>
   )
 }
